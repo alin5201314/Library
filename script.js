a41b388b0b9a5494f748ff4ae1ac8cdbddcbb147
alin5201314 <70863174+alin5201314@users.noreply.github.com>
2024-02-23 14:23:53 -0600

    Added adding card functionality

--- script.js
+++ script.js
@@ -1,50 +1,12 @@
-function makeBookCards(cardContainer){
-    for(let i = 0; i < myBooks.length;i++){
-        let grid = document.createElement("div")
-        grid.classList.add('bookCard')
-
-        let cardTitle = document.createElement("h1");
-        cardTitle.textContent = myBooks[i].title;
-        grid.appendChild(cardTitle);
-
-        let cardAuthor = document.createElement("p");
-        cardAuthor.textContent = "Author: " + myBooks[i].author;
-        grid.appendChild(cardAuthor);
-
-        let cardPages = document.createElement("p");
-        cardPages.textContent = "Pages: " + myBooks[i].pages;
-        grid.appendChild(cardPages);
-
-        let cardRead = document.createElement("p");
-        cardRead.textContent = "Read: " + (myBooks[i].read ? "Yes, I Finished" : "No, Never Read This");
-        grid.appendChild(cardRead);
-
-        let cardButtonContainer = document.createElement("div")
-        cardButtonContainer.classList.add("cardButtonsContainer")
-
-        let removeBook = document.createElement("button")
-        removeBook.textContent = "Remove Book";
-        let readBook = document.createElement("button")
-        readBook.textContent = "Complete"
-
-        cardButtonContainer.appendChild(removeBook)
-        cardButtonContainer.appendChild(readBook)
-
-        grid.appendChild(cardButtonContainer)
-        cardContainer.appendChild(grid)
-        cardContainer.append()
-    }
-
+// Book Constructor
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
 }
 
-function Book(title,author,pages,read) {
-    // the constructor...
-    this.title = title
-    this.author = author
-    this.pages = pages
-    this.read = read
-  }
-
+// Sample Data
 const titles = [
     "1984",
     "To Kill a Mockingbird",
@@ -86,26 +48,67 @@ const authors = [
 
 const read = [false,true,false,true,false,false,false,false,false,false]
 
-
 const myBooks = []
 
 for (let i = 0; i < authors.length; i++) {
     myBooks.push(new Book(titles[i], authors[i], pages[i], read[i]));
 }
 
+// Function Definitions
+function makeBookCard(cardContainer,book){
+    let grid = document.createElement("div")
+    grid.classList.add('bookCard')
+
+    let cardTitle = document.createElement("h1");
+    console.log(book);
+    cardTitle.textContent = book.title;
+    grid.appendChild(cardTitle);
+
+    let cardAuthor = document.createElement("p");
+
+    cardAuthor.textContent = "Author: " + book.author;
+    grid.appendChild(cardAuthor);
+
+    let cardPages = document.createElement("p");
+    cardPages.textContent = "Pages: " + book.pages;
+    grid.appendChild(cardPages);
+
+    let cardRead = document.createElement("p");
+    cardRead.textContent = "Read: " + (book.read ? "Yes, I Finished" : "No, Never Read This");
+    grid.appendChild(cardRead);
+
+    let cardButtonContainer = document.createElement("div")
+    cardButtonContainer.classList.add("cardButtonsContainer")
+
+    let removeBook = document.createElement("button")
+    removeBook.classList.add("removeBookButton")
+    removeBook.textContent = "Remove Book";
+    let readBook = document.createElement("button")
+    readBook.textContent = "Complete"
+
+    cardButtonContainer.appendChild(removeBook)
+    cardButtonContainer.appendChild(readBook)
+
+    grid.appendChild(cardButtonContainer)
+    cardContainer.appendChild(grid)
+    cardContainer.append()
+}
 
 
-console.log(myBooks);
+function makeBookCards(cardContainer){
+    for(let i = 0; i < myBooks.length;i++){
+        makeBookCard(cardContainer,myBooks[i])
+    }
+}
 
-const cardContainer = document.querySelector('div.container > div.content > div.contentCards');
-makeBookCards(cardContainer)
-console.log(cardContainer);
 
 
 
+// Event Listeners
 const dialog = document.querySelector("dialog");
 const showButton = document.querySelector("dialog + button");
 const closeButton = document.querySelector(".bookButtonClose");
+
 
 // "Show the dialog" button opens the dialog modally
 showButton.addEventListener("click", () => {
@@ -124,9 +127,12 @@ form.addEventListener('submit', function(event) {
     event.preventDefault();  // Prevent the default form submission behavior
     console.log(bookInfo = document.getElementById("bookForm").elements); 
     let isFull = true
+    let i = 0 
     for (let info of bookInfo) {
-        if (info.type !== 'checkbox' && info.value === '') {
-            isFull = false;
+        console.log(info.type);
+        console.log(info.value);
+        if (info.type !== 'checkbox' && info.type !== 'submit' && info.value === '') {
+            isFull = false
             break;
         }
 
@@ -134,6 +140,9 @@ form.addEventListener('submit', function(event) {
     if (isFull) {
         let readStatus = bookInfo['read'].checked; // true if checked, false otherwise
         myBooks.push(new Book(bookInfo['title'].value, bookInfo['author'].value, bookInfo['pages'].value, readStatus));
+        console.log("here");
+        console.log(myBooks);
+        makeBookCard(cardContainer,myBooks[myBooks.length - 1])
     }
     console.log(myBooks);
     if(isFull){
@@ -142,6 +151,47 @@ form.addEventListener('submit', function(event) {
     }
 });
 
+
+// Initialization
+console.log(myBooks);
+const cardContainer = document.querySelector('div.container > div.content > div.contentCards');
+makeBookCards(cardContainer);
+console.log(cardContainer);
+
+const removeButtons = document.querySelectorAll(".removeBookButton")
+
+removeButtons.forEach(removeButton => {
+    removeButton.addEventListener('click',() => {
+        let bookCard = removeButton.parentElement.parentElement;
+        let titleElement = bookCard.querySelector('h1'); 
+        let authorElement = bookCard.querySelector('p:nth-child(2)'); 
+        let authorName = authorElement.textContent.replace('Author: ', '');
+        for(let i = 0; i < myBooks.length; i++){
+            // console.log(`${myBooks[i].title}   ${titleElement.textContent}`);
+            console.log(`${myBooks[i].title } ${titleElement.textContent}`);
+            console.log(`${myBooks[i].author } ${authorElement.textContent}`);
+            if(myBooks[i].title === titleElement.textContent && myBooks[i].author === authorName){
+                console.log("here");
+                myBooks.splice(i, 1);
+                break; // Once you find the book and remove it, exit the loop
+            }
+        }
+        console.log(myBooks);
+
+        bookCard.remove()
+    })
+});
+
+// console.log(removeButtons);
+// for(j of removeButtons.pa){
+//     console.log((j));
+// }
+
+// const grids = document.querySelectorAll(".bookCard").values()
+// for(j of grids){
+//     console.log((j));
+// }
+// console.log(grids);
 
 
 
